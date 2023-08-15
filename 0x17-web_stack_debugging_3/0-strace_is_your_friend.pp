65cce55fce289ffea9b0080b27792b454b9d0d39
alexanderushe <alex.f.ushe@gmail.com>
2023-08-15 01:42:25 -0700

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes the `phpp` extensions to `php`
 
-exec { 'fixing-wordpress':
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
